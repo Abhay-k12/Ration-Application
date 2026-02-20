7f3deec2291b9fa632214c27a828bd95ff206f77
Abhay-k12 <abhaykanojia51@gmail.com>
2026-02-20 20:31:01 +0530

    Initialized main logic directory of application

---
+++ RationApplication/src/main/java/com/rationApplication/RationApplication/RationApplication.java
@@ -0,0 +1,13 @@
+package com.rationApplication.RationApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RationApplication.class, args);
+	}
+
+}
