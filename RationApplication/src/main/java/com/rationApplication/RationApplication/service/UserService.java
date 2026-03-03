525361b26e07d401b3f15589f9bb643c79fb2253
Abhay-k12 <abhaykanojia51@gmail.com>
2026-03-03 19:55:03 +0530

    Added service module of business logic

---
+++ RationApplication/src/main/java/com/rationApplication/RationApplication/service/UserService.java
@@ -0,0 +1,12 @@
+package com.rationApplication.RationApplication.service;
+
+import com.rationApplication.RationApplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+}
