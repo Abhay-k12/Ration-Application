6722ee03ea501826dffb35b6a9320b204e4316b1
Abhay-k12 <abhaykanojia51@gmail.com>
2026-03-05 13:34:17 +0530

    Added controleers for REST API'S

--- RationApplication/src/main/java/com/rationApplication/RationApplication/controller/BeneficiaryController.java
+++ RationApplication/src/main/java/com/rationApplication/RationApplication/controller/BeneficiaryController.java
@@ -1,15 +1,43 @@
 package com.rationApplication.RationApplication.controller;
 
+import com.rationApplication.RationApplication.entity.Beneficiary;
+import com.rationApplication.RationApplication.entity.User;
+import com.rationApplication.RationApplication.repository.UserRepository;
 import com.rationApplication.RationApplication.service.BeneficiaryService;
+import com.rationApplication.RationApplication.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/beneficiary")
+@Slf4j
 public class BeneficiaryController {
 
     @Autowired
     private BeneficiaryService beneficiaryService;
+
+    @Autowired
+    private UserService userService;
+
+    @PutMapping("/register")
+    @Transactional
+    public ResponseEntity<Boolean> addNewBeneficiary(@RequestBody Beneficiary beneficiary) {
+        try {
+            userService.addNewBeneficiaryUser(new User(beneficiary));
+            beneficiaryService.addNewBeneficiary(beneficiary);
+        }
+        catch(Exception e) {
+            log.error(e.toString());
+            throw e;
+        }
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
 
 }
--- RationApplication/src/main/java/com/rationApplication/RationApplication/controller/ComplaintController.java
+++ RationApplication/src/main/java/com/rationApplication/RationApplication/controller/ComplaintController.java
@@ -1,14 +1,35 @@
 package com.rationApplication.RationApplication.controller;
 
+import com.rationApplication.RationApplication.entity.Complaint;
+import com.rationApplication.RationApplication.entity.User;
 import com.rationApplication.RationApplication.service.ComplaintService;
+import com.rationApplication.RationApplication.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/complaint")
+@Slf4j
 public class ComplaintController {
 
     @Autowired
     private ComplaintService complaintService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Boolean> registerNewComplaint(@RequestBody Complaint complaint) {
+        try {
+            complaintService.registerComplaint(complaint);
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        }
+        catch(Exception e) {
+            log.error("ERROR in registering complaint: {}",e.toString());
+            return new ResponseEntity<>(false, HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
 }
