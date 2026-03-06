f8acebf7dc5c35071bdedcb524a2675bd386cfb6
Abhay-k12 <abhaykanojia51@gmail.com>
2026-03-06 14:18:14 +0530

    Updation into Aadhar and User Repository

--- RationApplication/src/main/java/com/rationApplication/RationApplication/repository/AadhaarRepository.java
+++ RationApplication/src/main/java/com/rationApplication/RationApplication/repository/AadhaarRepository.java
@@ -4,4 +4,5 @@ import com.rationApplication.RationApplication.entity.Aadhaar;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AadhaarRepository extends MongoRepository<Aadhaar, String> {
+    public Aadhaar findByAadhaarNumber(String userAadhaarNumber);
 }
