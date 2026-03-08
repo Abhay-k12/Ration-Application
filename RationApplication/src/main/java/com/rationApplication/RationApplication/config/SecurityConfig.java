208e46a7e207455cf59aa5e88ddc5fd174849239
Abhay-k12 <abhaykanojia51@gmail.com>
2026-03-08 16:44:41 +0530

    Added Spring Security for authentication and authorization

--- RationApplication/src/main/java/com/rationApplication/RationApplication/config/SecurityConfig.java
+++ RationApplication/src/main/java/com/rationApplication/RationApplication/config/SecurityConfig.java
@@ -1,59 +1,59 @@
-//package com.rationApplication.RationApplication.config;
-//
-//import com.rationApplication.RationApplication.service.CustomUserServiceImpl;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Autowired
-//    private CustomUserServiceImpl userDetailService;
-//
-//    /* Security filter chain : used instead of protected void configure(HttpSecurity http) throws Exception {}*/
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/admin/**","/beneficiary/**","/distributor/**","/complaint/**").authenticated()
-//                        .requestMatchers("/admin/**","/complaints/**").hasRole("ADMIN")
-//                        .requestMatchers("/distributor/**").hasRole("DISTRIBUTOR")
-//                        .requestMatchers("/beneficiary/**").hasRole("BENEFICIARY")
-//                        .anyRequest().permitAll()
-//                )
-//                .httpBasic(Customizer.withDefaults());
-//
-//        http.csrf(AbstractHttpConfigurer::disable);   //For stateless application it is okay to have csrf disable.
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public DaoAuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-//        provider.setUserDetailsService(userDetailService);
-//        provider.setPasswordEncoder(passwordEncoder());
-//        return provider;
-//    }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-//        return config.getAuthenticationManager();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package com.rationApplication.RationApplication.config;
+
+import com.rationApplication.RationApplication.service.CustomUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private CustomUserServiceImpl userDetailService;
+
+    /* Security filter chain : used instead of protected void configure(HttpSecurity http) throws Exception {}*/
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/**","/beneficiary/**","/distributor/**","/complaint/**").authenticated()
+                        .requestMatchers("/admin/**","/complaints/**").hasRole("ADMIN")
+                        .requestMatchers("/distributor/**").hasRole("DISTRIBUTOR")
+                        .requestMatchers("/beneficiary/**").hasRole("BENEFICIARY")
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);   //For stateless application it is okay to have csrf disable.
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
