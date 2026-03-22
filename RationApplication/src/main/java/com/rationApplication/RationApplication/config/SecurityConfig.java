aded51b31424f1410c3b84fd68a78ef4853b8c59
Abhay-k12 <abhaykanojia51@gmail.com>
2026-03-22 17:52:46 +0530

    Added JWT token based Web security

--- RationApplication/src/main/java/com/rationApplication/RationApplication/config/SecurityConfig.java
+++ RationApplication/src/main/java/com/rationApplication/RationApplication/config/SecurityConfig.java
@@ -1,59 +1,100 @@
 package com.rationApplication.RationApplication.config;
 
-import com.rationApplication.RationApplication.service.CustomUserServiceImpl;
+import com.rationApplication.RationApplication.security.JwtAuthenticationEntryPoint;
+import com.rationApplication.RationApplication.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private CustomUserServiceImpl userDetailService;
+    private UserDetailsService userDetailsService;
 
-    /* Security filter chain : used instead of protected void configure(HttpSecurity http) throws Exception {}*/
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/admin/**","/beneficiary/**","/distributor/**","/complaint/**").authenticated()
-                        .requestMatchers("/admin/**","/complaints/**").hasRole("ADMIN")
-                        .requestMatchers("/distributor/**").hasRole("DISTRIBUTOR")
-                        .requestMatchers("/beneficiary/**").hasRole("BENEFICIARY")
-                        .anyRequest().permitAll()
-                )
-                .httpBasic(Customizer.withDefaults());
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-        http.csrf(AbstractHttpConfigurer::disable);   //For stateless application it is okay to have csrf disable.
-        return http.build();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailService);
-        provider.setPasswordEncoder(passwordEncoder());
-        return provider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+
+                .antMatchers("/").permitAll()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/login_page.html").permitAll()
+                .antMatchers("/static/**").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/js/**").permitAll()
+                .antMatchers("/images/**").permitAll()
+                .antMatchers("/*.css").permitAll()
+                .antMatchers("/*.js").permitAll()
+
+                .antMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
+                .antMatchers(HttpMethod.GET, "/auth/validate").permitAll()
+
+                .antMatchers(HttpMethod.POST, "/complaint/register").permitAll()
+                .antMatchers(HttpMethod.PUT, "/complaint/resolveComplaint/**").permitAll()
+                .antMatchers(HttpMethod.PUT, "/complaint/rejectComplaint/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/complaint/**").permitAll()
+
+                .antMatchers("/beneficiary/**").hasRole("BENEFICIARY")
+                .antMatchers("/distributor/**").hasRole("DISTRIBUTOR")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
