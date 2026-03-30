e8dfaa43f37a4588ab0e3fc41e25844ea3599133
Abhay-k12 <abhaykanojia51@gmail.com>
2026-03-31 00:09:18 +0530

    Implement Data Transfer Object for API Responses

--- RationApplication/src/main/java/com/rationApplication/RationApplication/dto/ApiResponse.java
+++ RationApplication/src/main/java/com/rationApplication/RationApplication/dto/ApiResponse.java
@@ -1,4 +1,18 @@
 package com.rationApplication.RationApplication.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiResponse {
+    private boolean success;
+    private String message;
+
+    public ApiResponse(String message) {
+        this.message = message;
+        this.success = false;
+    }
 }
