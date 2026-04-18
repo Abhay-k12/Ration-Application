a4fded280671cf7df4a3f13624295c7d537b17cd
Abhay-k12 <abhaykanojia51@gmail.com>
2026-04-18 17:42:42 +0530

    Added method to allocate goods on the basis of district level schemes

--- RationApplication/src/main/java/com/rationApplication/RationApplication/repository/SchemeRepository.java
+++ RationApplication/src/main/java/com/rationApplication/RationApplication/repository/SchemeRepository.java
@@ -4,5 +4,8 @@ import com.rationApplication.RationApplication.entity.Scheme;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface SchemeRepository extends MongoRepository<Scheme, ObjectId> {
+    List<Scheme> findByStateDistrictCode(String stateDistrictCode);
 }
